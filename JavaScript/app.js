5d80da14bf39763a2ffe12bb7c0ad99b64f03d9e
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-03-02 15:53:56 -0500

    Added temp places to model, search box

---
+++ JavaScript/app.js
@@ -0,0 +1,26 @@
+var model = [
+    {
+        name: 'place1',
+        lat: 111,
+        lng: 111
+    },
+    {
+        name: 'place2',
+        lat: 222,
+        lng: 222
+    },
+    {
+        name: 'place3',
+        lat: 333,
+        lng: 333
+    },
+    {
+        name: 'place4',
+        lat: 444,
+        lng: 444
+    }
+];
+
+var viewModel = {
+    search: ko.observable("")
+};
