fb538d5731ce0f458b66cf3c08a929e495782964
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-03-03 19:14:54 -0500

    Added Map

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,6 +1,24 @@
-// Model
-var places = [
-    {
+// google maps init
+var map;
+function initMap() {
+    var santaCruz = {
+        lat: 36.9741171,
+        lng: -122.0329903
+    };
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: santaCruz,
+        mapTypeControl: false
+    });
+
+    infoWindow = new google.maps.InfoWindow();
+
+    bounds = new google.maps.LatLngBounds();
+
+    ko.applyBindings(viewModel);
+}
+// Location Model
+var places = [{
         name: 'place1',
         lat: 111,
         lng: 111
@@ -17,6 +35,11 @@ var places = [
     },
     {
         name: 'place4',
+        lat: 444,
+        lng: 444
+    },
+    {
+        name: 'place5',
         lat: 444,
         lng: 444
     }
