6f299dcc68882f08114b415e085b9a09df92b5d3
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-04-30 00:11:01 -0400

    Completely fixed infowindows when clikced on map

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -45,48 +45,68 @@ function initMap() {
     bounds = new google.maps.LatLngBounds();
 }
 
+function attachListener(marker, content){
+    marker.addListener('click', function () {
+        infoWindow.setContent(content);
+        infoWindow.open(marker.map, marker);
+    });
+}
+
 // Initializes infowindow with data
+// TODO: Self is causing all the options to be one location -> Need to change 'self' positioning
 function initInfoWindow(marker) {
 
     var self = this;
     this.lat = marker.getPosition().lat();
     this.lng = marker.getPosition().lng();
+
+    this.name = '';
+    this.city = '';
+    this.contact = '';
+    this.address = '';
+
     var clientId = "UXTL0ME5FM14HX40LDSXO5OUOMJYRDOU1PDYECVGVYWZ1ETG";
     var clientSecret = "YN2CEQJKAAU5W01O4VTPO0P3PYCKJ4QAGEJEEHPJO5DSLDX0";
-    var version = "20180425";
-    var foursquare_url = "https://api.foursquare.com/v2/venues/search?ll=" + this.lat + "," + this.lng + "&client_id=" + clientId + "&client_secret=" + clientSecret + "&query=" + marker.name + "&v=" + version;
+    var version = "20180429";
+    var foursquare_url = "https://api.foursquare.com/v2/venues/search?ll=" + this.lat + "," + this.lng + "&client_id=" + clientId + "&client_secret=" + clientSecret + "&query=" + marker.title + "&v=" + version;
 
+    // Reads the JSON response from foursquare, puts data into infowindow
     $.getJSON(foursquare_url).done(function (data) {
         var result = data.response.venues[0];
-
-      //  self.URL = result.url;
-        console.log(data.response.venues[0]);
-        self.name = marker.name;
-        self.address = result.location.formattedAddress[0] || 'No location provided';
+        self.result = data.url;
+        self.name = marker.title;
+        try {
+            self.address = result.location.formattedAddress[0]
+        } catch (err) {
+            'No location provided'
+        }
+        //self.address = result.location.formattedAddress[0] || 'No location provided';
         self.city = result.location.formattedAddress[1] || 'No city provided';
         try {
             self.contact = result.contact.phone;
-        }
-        catch(err) {
+        } catch (err) {
             self.contact = 'No phone number provided';
         }
-       // self.contact = result.contact.phone || 'No phone number provided';
-    }).fail(function(){
+
+        // self.contact = result.contact.phone || 'No phone number provided';
+        /* console.log(self.name + " is self.name inside");
+        console.log(self.address + " is self.address inside");
+        console.log(self.city + " is self.city inside"); */
+        console.log(marker.title + " is the marker");
+
+        self.content = "<div><b>" + self.name + "</b></div>" + "<div><b>" + self.address + "</b></div>" + "<div><b>" + self.city + "</b></div>" + "<div><b>" + self.contact + "</b></div>";
+
+        attachListener(marker, self.content);
+
+    }).fail(function () {
         alert("A problem has occurred with the foursquare api. Please refresh the page to continue.")
     });
 
     // Testing
-    console.log(self.name + " is self.name");
+    /* console.log(self.name + " is self.name");
     console.log(self.address + " is self.address");
-    console.log(self.city); //This is repeating --> Constantly shows rev
-
-
-    var content = "<div><b>" + self.name + "</b></div>" + "<div><b>" + self.address + "</b></div>" + "<div><b>" + self.city + "</b></div>" + "<div><b>" + self.contact + "</b></div>";
-
-    marker.addListener('click', function () {
-        infoWindow.setContent(content);
-        infoWindow.open(map, marker);
-    });
+    console.log(self.city + " is self.city");
+    console.log("self.content is       : " + self.content); */
 
 }
 
@@ -122,7 +142,7 @@ var viewModel = function () {
             })()
         };
         self.list().push(location);
-        console.log(location.marker.title + " successfully added");
+        //console.log(location.marker.title + " successfully added");
     }
 
     places.forEach(initMarkers);
@@ -152,6 +172,8 @@ var viewModel = function () {
     // to allow for use of 'self.content(?)'
     self.openIW = function (location) {
 
+        // TODO: Don't remake content, use other function.
+        // This should only open infoWindows
         var marker = location.marker;
         var map = marker.map;
         infoWindow.setContent(marker.title);
